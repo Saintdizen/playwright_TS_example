f28535c3bfdc9cf0402c9d059d5a7885e8bba0f9
syzoth <saintdizen@gmail.com>
2024-11-25 11:10:45 +0300

    test 27

--- framework/base.spec.ts
+++ framework/base.spec.ts
@@ -27,21 +27,68 @@ export class BasePage {
         await this.page.goto(path);
     }
 
+    basic() {
+        return this.page
+    }
+
     element(xpath: string): Element {
         return new Element(xpath, this.page);
     }
+
+    a(text: string): Element {
+        return new Link(text, this.page);
+    }
 }
 
-export class Element extends BasePage {
+class BaseElement extends BasePage {
     constructor(readonly locator: string, readonly page: Page) { super(page) }
 
+    // Действия
     async click(): Promise<void> {
         const page = await this.initPage()
         await page.locator(this.locator).click()
     }
 
+    async fill(text: string): Promise<void> {
+        const page = await this.initPage()
+        await page.locator(this.locator).fill(text)
+    }
+
+    //
+    async getCollection() {
+        const page = await this.initPage()
+        return await page.locator(this.locator).all()
+    }
+
+    // Проверки
     async toBeVisible(): Promise<void> {
         const page = await this.initPage()
         await expect(page.locator(this.locator)).toBeVisible()
+    }
+
+    async toBeHidden(): Promise<void> {
+        const page = await this.initPage()
+        await expect(page.locator(this.locator)).toBeHidden()
+    }
+
+    async toBeEnabled(): Promise<void> {
+        const page = await this.initPage()
+        await expect(page.locator(this.locator)).toBeEnabled()
+    }
+
+    async toBeDisabled(): Promise<void> {
+        const page = await this.initPage()
+        await expect(page.locator(this.locator)).toBeDisabled()
+    }
+}
+
+class Element extends BaseElement {
+    constructor(readonly locator: string, readonly page: Page) { super(locator, page) }
+}
+
+class Link extends BaseElement {
+    constructor(readonly text: string, readonly page: Page) { super(
+        `//a[text()='${text}'] | //a[@aria-label='${text}'] | //a[@id='${text}']`,
+        page)
     }
 }
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -7,13 +7,21 @@ test.describe('example', () => {
     test('example', async ({pages}) => {
         await pages.examplePage.open()
         //
-        await pages.examplePage.link.click()
+        await pages.examplePage.link1.click()
         await pages.examplePage.check.toBeVisible()
         await pages.examplePage.closePage()
         //
-        await pages.examplePage.link.click()
+        await pages.examplePage.link1.click()
         await pages.examplePage.check.toBeVisible()
         await pages.examplePage.check2.click()
         await pages.examplePage.check3.toBeVisible()
+        await pages.examplePage.closePage()
+        //
+        const links = pages.examplePage.links.getCollection()
+        for (let link of await links) {
+            console.log(await link.getAttribute('href'))
+        }
+        //
+        await pages.examplePage.link2.click()
     });
 });
